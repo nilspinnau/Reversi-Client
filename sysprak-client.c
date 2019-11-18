ea7197f6bf47a6e3fd3782cac9edcf92c7676a0a
Radu-Cristian Rusanu <rusanu@peridot.cip.ifi.lmu.de>
2019-11-18 15:15:20 +0100

    added getopt

--- sysprak-client.c
+++ sysprak-client.c
@@ -3,7 +3,9 @@
 #include <sys/types.h>
 #include <netdb.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 #include "performConnection.h"
 
 #define GAMEKINDNAME "Reversi"
@@ -11,18 +13,17 @@
 #define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
 
 int main(int argc, char **argv) {
-    long i = atoi(argv[1]);
-    if (argc != 2) {
-        printf("Bitte geben Sie eine 13-stellige Game-ID und die gewünschte Spielernummer an");
-        return -1;
-    }
-    if (i < 1 || i > 2) {
-        printf("Spielerzahl muss entweder 1 oder 2 sein");
-        return -1;
-    }
-    if (strlen(argv[0]) != 13) {
-        printf("Game-ID muss 13-stellig sein");
-        return -1;
+    int opt;
+    int gameId;
+    int playerNr;
+
+    while ((opt = getopt (argc, argv, "g:p:")) != -1) {
+        switch (opt) {
+            case 'g':
+            case 'p':
+                gameId = atof(argv[0]);
+                playerNr = atof(argv[1]);
+        }
     }
     int sockfd, newsockfd, portno, clilen, n;
     char buffer[256];
@@ -31,25 +32,30 @@ int main(int argc, char **argv) {
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     bzero((char* ) &serv_addr, sizeof(serv_addr));
     portno = PORTNUMBER;
+
+    struct hostent *server;
+    server = gethostbyname(HOSTNAME);
+    if (server == NULL) {
+        printf("ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr_list[0],
+        (char *)&serv_addr.sin_addr.s_addr,
+        server->h_length);
     serv_addr.sin_port = htons(portno);
-
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-        sizeof(serv_addr)) < 0)
-            printf("ERROR on binding");
-    listen(sockfd,5);
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    if (newsockfd < 0)
-        printf("ERROR on accept");
-
+    
+    printf("Please enter the message: ");
     bzero(buffer,256);
-    n = read(newsockfd,buffer,255);
-    if (n < 0) printf ("ERROR reading from socket");
-    printf("Here is the messsage: %s\n",buffer);
-    n = write(newsockfd,"I got your message", 18);
-    if (n < 0) printf ("ERROR writing to socket");
+    fgets(buffer,255,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0)
+        printf("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0)
+        printf("ERROR reading from socket");
+    printf("%s\n",buffer);
     return 0;
 }
