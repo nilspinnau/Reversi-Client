9be4e05925119915b2460ba391b6c1c5f1af1614
Nils Pinnau <pinnau@pyrit.cip.ifi.lmu.de>
2019-11-18 12:23:01 +0100

    updated source-files

--- sysprak-client.c
+++ sysprak-client.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include "performConnection.h"
+
+#define HOSTNUMBER (1357)
+#define GAMEKINDNAME ("Reversi")
+#define HOSTNAME ("sysprak.priv.lab.nm.ifi.lmu.de")
+
+
 
 int main(int argc, char **argv) {
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     
+    struct hostent *hstnm;
+    if((hstnm = gethostbyname(HOSTNAME)) == NULL) {
+        perror("gethostbyname");
+    }
+
+    performConnection();
+
+    exit(EXIT_SUCCESS);
 }
