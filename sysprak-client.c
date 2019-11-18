debe982984ad84eb5919e6cca2137b68edcee10a
Radu-Cristian Rusanu <rusanu@peridot.cip.ifi.lmu.de>
2019-11-18 13:57:17 +0100

    implemented socket

--- sysprak-client.c
+++ sysprak-client.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -25,11 +26,30 @@ int main(int argc, char **argv) {
     }
     int sockfd, newsockfd, portno, clilen, n;
     char buffer[256];
-    struct sockaddr_in, serv_addr, cli_addr;
+    struct sockaddr_in serv_addr, cli_addr;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero((char* ) &serv_addr, sizeof(serv_addr));
     portno = PORTNUMBER;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(portno);
+
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+        sizeof(serv_addr)) < 0)
+            printf("ERROR on binding");
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0)
+        printf("ERROR on accept");
+
+    bzero(buffer,256);
+    n = read(newsockfd,buffer,255);
+    if (n < 0) printf ("ERROR reading from socket");
+    printf("Here is the messsage: %s\n",buffer);
+    n = write(newsockfd,"I got your message", 18);
+    if (n < 0) printf ("ERROR writing to socket");
     return 0;
 }
