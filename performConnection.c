0c8aee14855290603d650d5cf15a84c6e3004438
Nils Pinnau <pinnau@pyrit.cip.ifi.lmu.de>
2019-11-18 14:34:04 +0100

    updated

--- performConnection.c
+++ performConnection.c
@@ -1,25 +1,29 @@
 #include "performConnection.h"
 
 
+// vielleicht noch fehlerbehandlung von send bearbeiten
 int performConnection(int socketfd) {
 
     float version = 2.0;
+    int GAME_ID = 0;
 
     char data[256];
 
-    while(recv(socketfd, data, sizeof(data), 0)) {
+    while(recv(socketfd, data, sizeof(data), 0) != 0) {
         switch(data[0]) {
             case '+':
                 printf("S: %s", data);
                 if(strstr(data, "+ MNM Gameserver")) {
+                    // sending client id/version
                     char answer[50];
                     sprintf(answer, "ID %.2f", version);
                     send(socketfd, answer, strlen(answer), 0);
                 } else if(strstr(data, "+ Client version accepted - please send Game-ID to join")) {
-
-                } else if(strstr(data, "+ PLAYING")) {
-
-                } else if(strstr(data, "+ Reversi")) {
+                    // sending gameID
+                    char answer[50];
+                    sprintf(answer, "%d", GAME_ID);
+                    send(socketfd, answer, strlen(answer), 0);
+                } else if(strstr(data, "+ PLAYING Reversi")) {
 
                 } else if(strstr(data, "+ YOU")) {
 
@@ -27,13 +31,28 @@ int performConnection(int socketfd) {
 
                 } else if(strstr(data, "+ ENDPLAYERS")) {
 
-                } else {
+                } else if(strstr(data, "+ WAIT")) {
+                    char answer[6] = "OKWAIT";
+                    send(socketfd, answer, strlen(answer), 0);
+                } else if(strstr(data, "+ MOVE")) {
 
-                }
+                } else if(strstr(data, "+ FIELD")) {
+
+                } else if(strstr(data, "+ ENDFIELD")) {
+                    // sending thinking answer
+                    char answer[8] = "THINKING";
+                    send(socketfd, answer, strlen(answer), 0);
+                } else if(strstr(data, "+ QUIT")) {
+                    // game ends -> servers sends quit signal
+                    perror(data);
+                    close(socketfd);
+                    exit(EXIT_SUCCESS);
+                } 
+
                 break;       
             default:
                 // disconnect
-                perror(data);            
+                printf("S: %s", data);            
                 close(socketfd);
                 exit(EXIT_FAILURE);
         }
