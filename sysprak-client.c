888ec3b225f0cadf7c73f49f4369136e80b9082f
Radu-Cristian Rusanu <rusanu@peridot.cip.ifi.lmu.de>
2019-11-18 12:41:56 +0100

    added constraints for command-line arguments

--- sysprak-client.c
+++ sysprak-client.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #define GAMEKINDNAME "Reversi"
 #define PORTNUMBER 1357
 #define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
 
 int main(int argc, char **argv) {
-    long i = strtol(argv[1], NULL, 10);
+    long i = atoi(argv[1]);
     if (argc != 2) {
         printf("Bitte geben Sie eine 13-stellige Game-ID und die gewünschte Spielernummer an");
         return -1;
@@ -20,5 +23,13 @@ int main(int argc, char **argv) {
         printf("Game-ID muss 13-stellig sein");
         return -1;
     }
+    int sockfd, newsockfd, portno, clilen, n;
+    char buffer[256];
+    struct sockaddr_in, serv_addr, cli_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    portno = PORTNUMBER;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
     return 0;
 }
