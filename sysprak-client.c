d55f394c5bc138b635158c6d2d88e057545a4b53
Hoang Giang <Giang.vu@campus.lmu.de>
2019-11-25 10:44:03 +0100

    new sysprak

--- sysprak-client.c
+++ sysprak-client.c
@@ -12,8 +12,6 @@
 #define PORTNUMBER 1357
 #define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
 
-void perror(const char *str) {}
-
 int main(int argc, char **argv) {
 
     int opt;
@@ -30,15 +28,19 @@ int main(int argc, char **argv) {
 	            break;
         }
     }
-    int sockfd, newsockfd, portno, clilen, n;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    bzero((char* ) &serv_addr, sizeof(serv_addr));
-    portno = PORTNUMBER;
-
+    int sockfd, portno; //,n;
+    struct sockaddr_in serv_addr;
     struct hostent *server;
+    
+    //char buffer[256];
+    portno = PORTNUMBER;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        perror("ERROR opening socket");
+    bzero((char* ) &serv_addr, sizeof(serv_addr));
+    
+
+    
     server = gethostbyname(HOSTNAME);
     if (server == NULL) {
         perror("ERROR, no such host\n");
@@ -46,22 +48,27 @@ int main(int argc, char **argv) {
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr_list[0],
+    bcopy((char *)server->h_addr,
         (char *)&serv_addr.sin_addr.s_addr,
-        server->h_length);
+        server-> h_length);
     serv_addr.sin_port = htons(portno);
-
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0)
-        perror("ERROR writing to socket");
-    bzero(buffer,256);
+    if (connect(sockfd,(struct sockaddr*) &serv_addr,sizeof(serv_addr)) < 0){
+        perror("ERROR connecting");
+    }
+    /*bzero(buffer,256);
+    fgets(buffer,255,stdin); 
+    
     n = read(sockfd,buffer,255);
     if (n < 0)
         perror("ERROR reading from socket");
     printf("%s\n",buffer);
+    */
     performConnection(sockfd);
+    
+    /*n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0)
+        perror("ERROR writing to socket");
+    bzero(buffer,256);*/
+    
     return 0;
 }
