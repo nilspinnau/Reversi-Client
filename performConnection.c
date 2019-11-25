aba4511f242ac583d4a29678f0ed78cff10734af
nilspinnau <nils.pinnau@live.de>
2019-11-25 12:40:48 +0100

    updated performConnection

--- performConnection.c
+++ performConnection.c
@@ -2,10 +2,9 @@
 
 
 // vielleicht noch fehlerbehandlung von send bearbeiten
-int performConnection(int socketfd) {
+int performConnection(int socketfd, char *gameId, int playerNr) {
 
-    float version = 2.0;
-    char GAME_ID[13];
+    char *version = "2.3";
 
     char data[256];
 
@@ -14,54 +13,37 @@ int performConnection(int socketfd) {
             case '+':
                 printf("S: %s", data);
 
-                // part of Spielverlauf but implemented earlier to get a faster answer to Server
-                if(strstr(data, "+ WAIT")) {
-                    // sending okwait answer
-                    char answer[6] = "OKWAIT";
-                    send(socketfd, answer, strlen(answer), 0);
-                } else if(strstr(data, "+ ENDFIELD")) {
-                    // sending thinking answer
-                    char answer[8] = "THINKING";
-                    send(socketfd, answer, strlen(answer), 0);
-                    // signal send to Thinker process to figure out a move
-
-                // begining of Prolog
-                } else if(strstr(data, "+ MNM Gameserver")) {
+                if(strstr(data, "+ MNM Gameserver") != NULL) {
                     // sending client id/version
-                    char answer[3];
-                    sprintf(answer, "ID %.2f", version);
-                    send(socketfd, answer, strlen(answer), 0);
-                } else if(strstr(data, "+ Client version accepted - please send Game-ID to join")) {
-                    // sending gameID, is it an int or char???
-                    char answer[13];
-                    sprintf(answer, "%s", GAME_ID);
-                    send(socketfd, answer, strlen(answer), 0);
-                } else if(strstr(data, "+ PLAYING")) {
+                    char answer[30];
+                    sprintf(answer, "VERSION %s%s", version, "\n\0");
+                    send(socketfd, answer, sizeof(answer), 0);
+                    printf("C: %s", answer);
+                    
+                } else if(strstr(data, "+ Client version accepted - please send Game-ID to join") != NULL) {
+                    char id[30];
+                    sprintf(id, "ID %s%s", gameId, "\n\0");
+                    send(socketfd, id, sizeof(id), 0);
+                    printf("C: %s", id);
+
+                } else if(strstr(data, "+ PLAYING") != NULL) {
                     // nothing happens, server sends another message
-                } else if(strstr(data, "+ GAMENAME")) {
-                    char answer[6] = "PLAYER";
-                    send(socketfd, answer, strlen(answer), 0);
-                } else if(strstr(data, "+ YOU")) {
-                    // writes back Mitspielernummer and Mitspielername
-                } else if(strstr(data, "+ TOTAL")) {
-                    // writes back Mitspieleranzahl
-                } else if(strstr(data, "+ ENDPLAYERS")) {
-
-
-
-
-                   // end of Prolog, Spielverlauf begins
-                } else if(strstr(data, "+ MOVE")) {
-
-                } else if(strstr(data, "+ FIELD")) {
-
-                } else if(strstr(data, "+ QUIT")) {
-                    // game ends -> servers sends quit signal
-                    perror(data);
-                    close(socketfd);
+                
+                } else if(strstr(data, "+ GAMENAME") != NULL) {
+                    char player[30];
+                    sprintf(player, "PLAYER %i%s", playerNr, "\n\0");
+                    send(socketfd, player, sizeof(player), 0);
+                    printf("C: %s\n", player);
+                
+                } else if(strstr(data, "+ YOU") != NULL) {
+                    // server: Mitspielernummer and Mitspielername
+                
+                } else if(strstr(data, "+ TOTAL") != NULL) {
+                    // server: Mitspieleranzahl
+                
+                } else if(strstr(data, "+ ENDPLAYERS") != NULL) {
                     exit(EXIT_SUCCESS);
-                } 
-
+                }
                 break;       
             default:
                 // disconnect, error message from server
@@ -72,9 +54,4 @@ int performConnection(int socketfd) {
     }
     exit(EXIT_SUCCESS);
 }
-
-int main(){
-
-}
-
 
--- performConnection.h
+++ performConnection.h
@@ -9,4 +9,4 @@
 // Prolog Phase der Kommunikation
 // Zuweisung eines freien Spielers durch Gameserver
 // @param int socketfd: FIle-Deskriptor des Sockets
-int performConnection(int socketfd);
+int performConnection(int socketfd, char *gameId, int playerNr);
